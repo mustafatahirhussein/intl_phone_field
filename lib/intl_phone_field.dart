e41b61524d254230efae19387d56d9856f36c844
= <mustafatahirhussein@gmail.com>
2023-09-04 17:42:29 +0500

    changes

--- lib/intl_phone_field.dart
+++ lib/intl_phone_field.dart
@@ -456,8 +456,7 @@ class _IntlPhoneFieldState extends State<IntlPhoneField> {
                     child: Image.asset(
                       'assets/flags/${_selectedCountry.code.toLowerCase()}.png',
                       package: 'intl_phone_field',
-                      width: 30,
-                      height: 42,
+                      width: 42,
                     ),
                   ),
                   SizedBox(width: 8),
